be2a4eecba6d5ad19bc6b084206968ca487d2476
Thomas <thomassantospallarini@gmail.com>
2025-06-05 21:41:54 -0300

    feat(api)!: add fetch pokemon details

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,11 +1,18 @@
 const pokeApi = {};
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+} 
+
+
 pokeApi.getPokemons = (offset = 0 , limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
   return fetch(url)
     .then((response) => response.json())
-    .then((responseBody) => responseBody.results)
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
     .catch((error) => {
       console.error("Error fetching data:", error);
       throw error; // Re-throw the error for further handling if needed
