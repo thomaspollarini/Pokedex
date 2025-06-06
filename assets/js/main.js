2765137ffb035b1fd7c5ccc7a3dd165e71caf706
Thomas <thomassantospallarini@gmail.com>
2025-06-06 13:22:21 -0300

    feat: implement "Load more" button for incremental card loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,30 @@
-
-function getScreenPokemonLimit(){
-    const bodyWidth = document.body.clientWidth;
-    if (bodyWidth < 380){
-        return 5;
-    } else if (bodyWidth < 576){
-        return 10;
-    } else if (bodyWidth < 992){
-        return 15;
-    } else{
-        return 20;
-    }
+function getScreenPokemonLimit() {
+  const bodyWidth = document.body.clientWidth;
+  if (bodyWidth < 380) {
+    return 5;
+  } else if (bodyWidth < 576) {
+    return 10;
+  } else if (bodyWidth < 992) {
+    return 15;
+  } else {
+    return 20;
+  }
 }
 
 function convertPokemonToLi(pokemon) {
-return `<li class="pokemon ${pokemon.type}">
-            <span class="pokemon__number">#${pokemon.number.toString().padStart(3, '0')}</span>
+  return `<li class="pokemon ${pokemon.type}">
+            <span class="pokemon__number">#${pokemon.number
+              .toString()
+              .padStart(3, "0")}</span>
             <span class="pokemon__name">${pokemon.name}</span>
             <div class="pokemon__detail">
                     <ol class="pokemon__types">
-                            ${pokemon.types.map((type) => `<li class="pokemon__type">${type}</li>`).join("")}
+                            ${pokemon.types
+                              .map(
+                                (type) =>
+                                  `<li class="pokemon__type">${type}</li>`
+                              )
+                              .join("")}
                     </ol>
                     <img src="${pokemon.image}" alt="${pokemon.name}">
             </div>
@@ -26,13 +32,23 @@ return `<li class="pokemon ${pokemon.type}">
 `;
 }
 
+function loadPokemonItems(offset, limit) {
+  pokeApi
+    .getPokemons(offset, limit)
+    .then(
+      (pokemonList = []) =>
+        (pokedexList.innerHTML += pokemonList.map(convertPokemonToLi).join(""))
+    );
+}
+
 const pokedexList = document.querySelector(".pokemons");
+const loadMoreButton = document.querySelector(".load-more");
+const limit = getScreenPokemonLimit();
+let offset = 0;
 
+loadPokemonItems(offset, limit);
 
-
-pokeApi
-  .getPokemons(0, getScreenPokemonLimit())
-  .then(
-    (pokemonList = []) =>
-      (pokedexList.innerHTML = pokemonList.map(convertPokemonToLi).join(""))
-  );
+loadMoreButton.addEventListener("click", () => {
+offset += limit;
+  loadPokemonItems(offset);
+});
