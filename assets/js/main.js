64fbd3ce6fd8f4bdf50c26f9e326283f38c0f715
Thomas <thomassantospallarini@gmail.com>
2025-06-06 11:20:34 -0300

    feat: calculate visible Pokemon cards for API requests

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,18 @@
 
+function getScreenPokemonLimit(){
+    const bodyWidth = document.body.clientWidth;
+    if (bodyWidth < 380){
+        return 5;
+    } else if (bodyWidth < 576){
+        return 10;
+    } else if (bodyWidth < 992){
+        return 15;
+    } else{
+        return 20;
+    }
+}
+
 function convertPokemonToLi(pokemon) {
-    console.log(pokemon);
 return `<li class="pokemon">
             <span class="pokemon__number">#${pokemon.number.toString().padStart(3, '0')}</span>
             <span class="pokemon__name">${pokemon.name}</span>
@@ -16,8 +28,10 @@ return `<li class="pokemon">
 
 const pokedexList = document.querySelector(".pokemons");
 
+
+
 pokeApi
-  .getPokemons()
+  .getPokemons(0, getScreenPokemonLimit())
   .then(
     (pokemonList = []) =>
       (pokedexList.innerHTML = pokemonList.map(convertPokemonToLi).join(""))
